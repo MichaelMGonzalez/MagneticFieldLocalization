52b9be9f70d02d5414c21ef2a954beadd32f7ecd
John Doe <johndoe@example.com>
2016-06-05 16:53:29 -0700

    Android magnetometer
    Is displaying Magnetometer values (magnitude of vector) from the
    calibrated magnetic filed sensor type. Starting Timer class.

--- MagneticFieldLocalizer/app/src/main/java/com/ashmp/magneticfieldlocalizer/MainActivity.java
+++ MagneticFieldLocalizer/app/src/main/java/com/ashmp/magneticfieldlocalizer/MainActivity.java
@@ -4,8 +4,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -22,11 +24,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         teslaView = (TextView) findViewById(R.id.edit_tesla);
         timeView = (TextView) findViewById(R.id.edit_time);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
-    public void onSensorChanged(SensorEvent se) {
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        float[] magnetic_vector = event.values;
+
+        float magnitude = (float)Math.sqrt(Math.pow(magnetic_vector[0],2)+Math.pow(magnetic_vector[1],2)+Math.pow(magnetic_vector[2],2));
+        teslaView.setText(Float.toString(magnitude));
+       // teslaView.setText("I GOT CALLED TOO");
         // Will be used I think
+    }
+
+    public void startTime(View view) {
+
     }
     public void onAccuracyChanged(Sensor s, int i) {
         //not used - needed here because it's and abstract method in SensorEventListener
---
+++ MagneticFieldLocalizer/app/src/main/java/com/ashmp/magneticfieldlocalizer/Timer.java
@@ -0,0 +1,17 @@
+package com.ashmp.magneticfieldlocalizer;
+
+import java.util.Calendar;
+
+/**
+ * Created by Ashmp on 6/5/2016.
+ */
+public class Timer {
+    long startTime = 0;
+
+    void setStartTime() {
+        startTime = Calendar.get(Calendar.getInstance(), Calendar.MILLISECOND);
+    }
+    long getTime() {
+        return (Calendar.get(Calendar.getInstance(), Calendar.MILLISECOND) - startTime)
+    }
+}
