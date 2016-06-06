94c79afa04b8ca590bfcceaa15359442da424269
MichaelMGonzalez <mmg005@ucsd.edu>
2016-06-05 23:51:05 -0700

    python communicator can pick good inputs to read

--- SerialCommunication/Communicator.py
+++ SerialCommunication/Communicator.py
@@ -1,7 +1,13 @@
 import serial
 import os
 import struct
+import json
+import sys
 
+file_path = "SerialMsgHeader.json"
+
+f = open(file_path)
+json_obj = json.load(f)
 
 port_name = ""
 if os.name == "nt":
@@ -10,7 +16,12 @@ if os.name == "nt":
     print "Trying to connect to:", port_name
 else: print "OS NOT SUPPORTED"
 
-communicator = serial.Serial(port_name, 9600)
+value_to_msg_map = dict( [int(v["value"]),v["name"]] for v in json_obj["msgs"] )
+msg_sent = struct.pack( "c", chr([ v["value"] for v in json_obj["msgs"] if v["name"] == "MSG_SENT" ][0]  & 255) )
+baud_rate = int([ o["value"] for o in json_obj["setup"] if o["name"] == "BAUD_RATE" ][0])
+
+
+communicator = serial.Serial(port_name, baud_rate)
 
 print "Would you like to read or write? (r/w)"
 state = None
@@ -26,9 +37,12 @@ while not state:
         
 print "You have selected: ", state
 while state == "reading":
-    if communicator.in_waiting >= 4: 
-         
-        bs = communicator.read(size=4)
-        #print bs
-        print struct.unpack("f",bs)[0]
+    if communicator.in_waiting >=6: 
+        f_b = ord(communicator.read())
+        #print f_b
+        if f_b in value_to_msg_map:
+            bs = communicator.read(size=4)
+            sanity_byte = communicator.read()
+            if sanity_byte == msg_sent: 
+                print value_to_msg_map[f_b], struct.unpack("f",bs)[0]
 exit()
