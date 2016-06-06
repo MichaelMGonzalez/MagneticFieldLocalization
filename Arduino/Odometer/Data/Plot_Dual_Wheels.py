e8647f57e77b77ae2fb95b7864a3a6ec427abfd9
MichaelMGonzalez <mmg005@ucsd.edu>
2016-06-05 19:37:50 -0700

    more data crunching

--- Arduino/Odometer/Data/Plot_Dual_Wheels.py
+++ Arduino/Odometer/Data/Plot_Dual_Wheels.py
@@ -2,7 +2,8 @@ import csv
 import sys
 #import numpy as np
 import matplotlib.pyplot as plt
-
+from Plot_Histogram import *
+import math as m
 
 
 # Right Rising Edge
@@ -62,11 +63,33 @@ def plot():
 l_hits, l_is = count_odom_hits( left_vals, l_r_edge, l_f_edge)
 r_hits, r_is = count_odom_hits( right_vals, r_r_edge, r_f_edge)
 
+l_times = [ times[i] for i in l_is ]
+r_times = [ times[i] for i in r_is ]
+
+def extract_rpm( ts ):
+    sig_is = [ i for i in range(len(ts)) if i != 0 and i % 5 == 0 ]
+    last_stamp = ts[0]
+    rpm = []
+    for i in range(1,len(ts)):
+    #for i in sig_is:
+        rpm.append( 60.0 / ( (ts[i] - last_stamp) ) / 5  )
+        last_stamp = ts[i]
+    return rpm
+
+def rpm_to_rad_p_s( t ):
+    return 2*(m.pi) * t/60.0
+def rpm_to_v( t ):
+    return rpm_to_rad_p_s(t) * .068/2
 
 
-print "Left Spoke Hits: ", l_is
+l_rpm = extract_rpm(l_times)
+r_rpm = extract_rpm(r_times)
+print "Left Spoke Hits: ", l_rpm
+show_hist(map( rpm_to_v,  l_rpm))
+show_hist(map( rpm_to_v,  r_rpm))
 
-print "Right Spoke Hits: ", r_is
+#print "Right Spoke Hits: ", 
+#extract_rpm(r_times)
 #plot()
 
 
--- Arduino/Odometer/Data/Plot_Histogram.py
+++ Arduino/Odometer/Data/Plot_Histogram.py
@@ -6,21 +6,27 @@ import math as m
 
 bin_size = 20
 
-if len( sys.argv ) == 1:
-    print "Please enter the csv file you want to plot!"
-    sys.exit(0)
 
-points = []
-with open( sys.argv[1] ) as csvfile:
-    reader = csv.reader(csvfile)
-    points = [ 2.0 * m.pi * (68.0)/2000 / (float(c[0]) / 1000000) for c in reader]
 
-print (points)
-xs = range( len( points ) )
 
-plt.hist( points, bin_size)
-plt.xlabel("Velocity as m/s")
-plt.ylabel("Probability")
-plt.title("Histogram of the time required for a full motor revolution")
-plt.suptitle("Sample size: n = " + str(len(xs )) + "\n" + "Bin Size: " + str(bin_size))
-plt.show()
+def show_hist(points):
+    plt.hist( points, bin_size)
+    plt.xlabel("Velocity as m/s")
+    plt.ylabel("Probability")
+    plt.title("Histogram of the time required for a full motor revolution")
+    plt.suptitle("Sample size: n = " + str(len(points )) + "\n" + "Bin Size: " + str(bin_size))
+    plt.show()
+
+    
+if __name__ == "__main__":
+    if len( sys.argv ) == 1:
+        print "Please enter the csv file you want to plot!"
+        sys.exit(0)
+
+    points = []
+    with open( sys.argv[1] ) as csvfile:
+        reader = csv.reader(csvfile)
+        points = [ 2.0 * m.pi * (68.0)/2000 / (float(c[0]) / 1000000) for c in reader]
+    print (points)
+    xs = range( len( points ) )
+    show_hist(points)
